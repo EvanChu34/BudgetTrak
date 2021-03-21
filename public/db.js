cc3ca099e85d6e8b0b337937fba8f66deff21809
Evan Chu <echu34@gmail.com>
2021-03-21 14:38:09 -0400

    adding to the db js

---
+++ public/db.js
@@ -0,0 +1,21 @@
+const pendingObjectStoreName = `pending`;
+
+const request = indexedDB.open(`budget`, 1);
+
+request.onupgradeneeded = event => {
+  const db = request.result;
+  console.log(event);
+
+  if (!db.objectStoreNames.contains(pendingObjectStoreName)) {
+    db.createObjectStore(pendingObjectStoreName, { autoIncrement: true });
+  }
+};
+
+request.onsuccess = event => {
+    console.log(`Success! ${event.type}`);
+    if (navigator.onLine) {
+      checkDatabase();
+    }
+  };
+  
+  request.onerror = event => console.error(event);
