0286a0b8a68455d927f36be30aa6dd59d02f6059
Evan Chu <echu34@gmail.com>
2021-03-21 12:30:19 -0400

    added a file and some service worker work

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,17 @@
 const FILES_TO_CACHE = [
-
-
-
+"/",
+"/index.html",
+"/db.js",
+"/index.js",
+"/manifest.webamainfest",
+"/style.css"
 ];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-self.addEventListener("install", function(evt) {
-    evt.waitUntil(
+self.addEventListener("install", (event) => {
+    event.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
         console.log("Your files were pre-cached successfully!");
         return cache.addAll(FILES_TO_CACHE);
@@ -17,4 +20,23 @@ self.addEventListener("install", function(evt) {
   
     self.skipWaiting();
 });
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keyList) => {
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME){
+            return caches.delete(key);
+          }
+          return undefined
+        })
+      
+      )
+  )
+})
+
+
+
+
 
